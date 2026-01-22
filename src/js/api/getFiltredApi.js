08ed502181503ffd7c9ef7cccbb67db40da73d4e
danilT9 <daniltrotsko9@gmail.com>
2026-01-22 18:26:31 +0300

    header

--- src/js/api/getFiltredApi.js
+++ src/js/api/getFiltredApi.js
@@ -1,11 +1,5 @@
 import { API_KEY } from "./getEventsApi";
 
-export const getCountriesByApi = async() => {
-    return await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${API_KEY}&size=200`)
-        .then(res => res.json())
-        .then(res => res._embedded.events);
-};
-
 export const getEventsByKeywordApi = async (page = 0, keyword, countryCode = "") => {
   const response = await fetch(
     `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${API_KEY}&page=${page}&size=20&keyword=${keyword}&countryCode=${countryCode}`
